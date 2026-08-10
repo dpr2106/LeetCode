76ce2c16563eed48555c67d74cc7834058ee13b0
Prashanth <87235698+dpr2106@users.noreply.github.com>
2026-08-10 20:26:18 +0530

    20. Valid Parentheses

---
+++ 0020_valid_paranthesis.cpp
@@ -0,0 +1,34 @@
+#include <stack>
+#include <string>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (int i = 0 ; i < s.length() ; i++) {
+            char c = s[i];
+
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            }
+            else {
+                if (st.empty()) {
+                    return false;
+                }
+                char top = st.top();
+
+                if ((c == ')' && top == '(') ||
+                    (c == '}' && top == '{') || 
+                    (c == ']' && top == '[')) {
+                    st.pop();
+                } else {
+                    return false;
+                }
+            }
+        }
+        return st.empty();
+    }
+};
